a2f9bc5354a047f89189239360632b68f697b1fa
Felipe <filorca@miuandes.cl>
2019-06-18 11:25:42 -0400

    seed 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home_controller/index'
   resources :orders
   resources :products
   resources :addresses
   devise_for :users
+  root to: 'home_controller#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
